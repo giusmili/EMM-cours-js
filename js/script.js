674b41064cf872740cb35adc8536e249e7d48a9a
giusmili <giusmili67@gmail.com>
2024-06-19 17:45:52 +0200

    javascript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const content = "Bonjour ici c'est javascript et toi t'es qui?";
+let result = content.split(" ");
+console.table(result);
+
+/* table */
+
+const lang = ["html", "css", true];
+console.table(lang);
+console.log(lang.length);
+
+lang.map(index => {
+    console.log(index);
+});
+
+for (let data in lang) {
+    console.log(`${data} : ${lang[data]}`);
+}
+
+const User = {
+    nom: "Doe",
+    prenom: "John",
+    ville: "NY",
+    pays: "USA"
+};
+console.table(User);
+console.log(Object.keys(User), Object.values(User));
