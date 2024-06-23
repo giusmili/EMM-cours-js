5fd6a8bd4f2e4c718dbc5c8adb63e8c3f54c0833
giusmili <giusmili67@gmail.com>
2024-06-23 18:32:34 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
-const content = "Bonjour ici c'est javascript et toi t'es qui?";
-let result = content.split(" ");
-console.table(result);
+(() => {
+  const content = "Bonjour ici c'est javascript et toi t'es qui?";
+  let result = content.split(" ");
+  console.table(result);
 
-/* table */
+  /* table */
 
-const lang = ["html", "css", true];
-console.table(lang);
-console.log(lang.length);
+  const lang = ["html", "css", true];
+  console.table(lang);
+  console.log(lang.length);
 
-lang.map(index => {
-  console.log(index);
-});
+  lang.map(index => {
+    console.log(index);
+  });
 
-for (let data in lang) {
-  console.log(`${data} : ${lang[data]}`);
-}
+  for (let data in lang) {
+    console.log(`${data} : ${lang[data]}`);
+  }
 
-const User = {
-  nom: "Doe",
-  prenom: "John",
-  ville: "NY",
-  pays: "USA"
-};
-console.table(User);
-console.log(Object.keys(User), Object.values(User));
+  const User = {
+    nom: "Doe",
+    prenom: "John",
+    ville: "NY",
+    pays: "USA"
+  };
+  console.table(User);
+  console.log(Object.keys(User), Object.values(User));
 
-for (let rows in User) {
-  console.log(`${rows} : ${User[rows]}`);
-}
+  for (let rows in User) {
+    console.log(`${rows} : ${User[rows]}`);
+  }
 
-/* tableau multidimensionnel */
-const product = [{ name: "car", price: 6000 }, { name: "Bike", price: 150 }, { name: "phone", price: 700 }, { name: "Watch", price: 200 }, { name: "pen", price: 8 }, { name: "bag", price: 50 }];
-console.table(product);
+  /* tableau multidimensionnel */
+  const product = [{ name: "car", price: 6000 }, { name: "Bike", price: 150 }, { name: "phone", price: 700 }, { name: "Watch", price: 200 }, { name: "pen", price: 8 }, { name: "bag", price: 50 }];
+  console.table(product);
 
-const res = product.filter(row => row.price > 8); /* méthodes : find, map et filter */
-console.table(res);
+  const res = product.filter(row => row.price > 8); /* méthodes : find, map et filter */
+  console.table(res);
+})();
